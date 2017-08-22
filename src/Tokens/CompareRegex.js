dd2c69ae242c173d0b8c65cdfdc8e66a1581f5fe
Peter Corcoran <peter@lemondigits.com>
2017-08-22 14:40:04 +0100

    Regex compare '~=' now accepts a string instead of a regex, this will run faster and without the regex engine involved.

--- src/Tokens/CompareRegex.js
+++ src/Tokens/CompareRegex.js
@@ -4,7 +4,15 @@ import {P_COMP} from '../Precedence';
 export default class CompareRegex extends TokenABOperator {
 
     constructor() {
-        super(/~=/, P_COMP, (a, b) => b.test(a));
+        super(/~=/, P_COMP, (a, b) => {
+			if(typeof b == 'regex')
+				return b.test(a);
+				
+			if(typeof b == 'string');
+				return a.indexOf(b) !== -1;
+
+			return false;
+		});
     }
 
 }
