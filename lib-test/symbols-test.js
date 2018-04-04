05904d80b210a8553095262b1ec9b37400fb5a58
Peter Corcoran <peter.corcoran@shadowacre.ltd>
2018-04-04 16:19:54 +0100

    Prepare for version 0.2.6

--- lib-test/symbols-test.js
+++ lib-test/symbols-test.js
@@ -37,7 +37,16 @@ _buster2.default.testCase("Symbols - Context", {
     },
 
     "simple": function simple() {
-        return _buster2.default.assert.equals(OE.evaluate("my_var", { my_var: 12345 }), 12345);
+        return _buster2.default.assert.equals(OE.evaluate("my_var_32", { my_var_32: 12345 }), 12345);
+    },
+    "simple (hypen)": function simpleHypen() {
+        return _buster2.default.assert.equals(OE.evaluate("my-var-32", { "my-var-32": 12345 }), 12345);
+    },
+    "underscore": function underscore() {
+        return _buster2.default.assert.equals(OE.evaluate("_", { "_": 12345 }), 12345);
+    },
+    "underscore start": function underscoreStart() {
+        return _buster2.default.assert.equals(OE.evaluate("_underscore", { "_underscore": 12345 }), 12345);
     },
     "deep": function deep() {
         return _buster2.default.assert.equals(OE.evaluate("test.with.depth", { test: { with: { depth: 54321 } } }), 54321);
@@ -45,7 +54,7 @@ _buster2.default.testCase("Symbols - Context", {
     "multi": function multi() {
         return _buster2.default.assert.equals(OE.evaluate("my_var + other_var", { my_var: 12345, other_var: 54321 }), 66666);
     },
-    "multi-deep": function multiDeep() {
+    "multi + deep": function multiDeep() {
         return _buster2.default.assert.equals(OE.evaluate("test.with.depth + test.with.other", { test: { with: { depth: 54321, other: 12345 } } }), 66666);
     }
 });
